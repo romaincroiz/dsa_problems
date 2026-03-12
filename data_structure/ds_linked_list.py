69009b9416a7652867ede4993d0c377e89c87709
Romain C. <croizier.romain@gmail.com>
2026-03-12 10:34:20 +0900

    fix: add len and peak methods + cleanup

--- data_structure/ds_linked_list.py
+++ data_structure/ds_linked_list.py
@@ -1,6 +1,3 @@
-from typing import Any
-
-
 class Node:
 
     def __init__(self, data):
@@ -11,12 +8,12 @@ class Node:
         return str(self.data)
 
 class DsLinkedList:
-    head = None
-    size = 0
+    __head = None
+    __size = 0
 
     def __str__(self):
         s = '['
-        current_node = self.head
+        current_node = self.__head
         is_first = True
         while current_node:
             if is_first:
@@ -31,37 +28,37 @@ class DsLinkedList:
     def get(self, position):
         self.__check_in_range(position)
         if position == 0:
-            return self.head
+            return self.__head
         current_node = self.__get_node_before(position)
         return current_node.next
 
-    def __check_in_range(self, index) -> bool | Any:
-        if not(0 <= index <= self.size):
+    def __check_in_range(self, index):
+        if not(0 <= index <= len(self)):
             raise Exception('index out of range')
 
-    def __get_node_before(self, position) -> Any:
+    def __get_node_before(self, position) -> Node:
         # iterate until just before the position
-        current_node = self.head
+        current_node = self.__head
         for i in range(0, position - 1):
             current_node = current_node.next
         return current_node
 
-    def remove_first(self, data):
+    def remove_first(self, data) -> bool:
         if self.is_empty() or data is None:
             return False
         return self.__find_and_remove_first(data)
 
     def __find_and_remove_first(self, data) -> bool:
         # Head: no need to iterate
-        if self.head.data == data:
+        if self.__head.data == data:
             self.__remove_head()
             return True
 
         return self.__iterate_and_remove_first(data)
 
     def __iterate_and_remove_first(self, data) -> bool:
-        current_node = self.head
-        next_node = self.head.next
+        current_node = self.__head
+        next_node = self.__head.next
         while next_node:
             if next_node.data == data:
                 current_node.next = next_node.next
@@ -82,14 +79,14 @@ class DsLinkedList:
     def __remove_head(self):
         if self.is_empty():
             return
-        self.__remove(None, self.head)
+        self.__remove(None, self.__head)
 
     def __remove(self, prev_node, node_to_delete):
         if node_to_delete is None:
             return
         if prev_node is None:
-            # head removed
-            self.head = self.head.next
+            # __head removed
+            self.__head = self.__head.next
         else:
             prev_node.next = node_to_delete.next
             node_to_delete.next = None
@@ -107,59 +104,81 @@ class DsLinkedList:
 
     def __insert(self, previous_node, new_node):
         if previous_node is None:
-            # new node becomes head
-            new_node.next = self.head
-            self.head = new_node
+            # new node becomes __head
+            new_node.next = self.__head
+            self.__head = new_node
         else:
             new_node.next = previous_node.next
             previous_node.next = new_node
         self.__increment_size()
 
-    def is_empty(self) -> Any:
-        return self.size == 0
+    def __len__(self):
+        return self.__size
+
+    def peak(self):
+        if not self.is_empty():
+            return self.__head
+        return None
+
+    def is_empty(self) -> bool:
+        return len(self) == 0
 
     def clear(self):
-        self.head = None
-        self.size = 0
+        self.__head = None
+        self.__size = 0
 
     def __decrement_size(self):
-        self.size -= 1
+        self.__size -= 1
 
     def __increment_size(self):
-        self.size += 1
+        self.__size += 1
 
 def test_linked_list():
-    ds = DsLinkedList()
+    ll = DsLinkedList()
 
-    ds.add("apple", 0)
-    print(ds) # [apple]
-    ds.add("orange", 0)
-    print(ds) # [orange->apple]
-    ds.add("strawberry", 1)
-    print(ds) # [orange->strawberry->apple]
-    print(f'Node at position 2: {ds.get(2)}')
-    ds.add("banana", 2)
-    print(ds) # [orange->strawberry->banana->apple]
-    ds.add("strawberry", 3)
-    print(ds) # [orange->strawberry->banana->strawberry->apple]
-    print(f'Node at position 3: {ds.get(3)}')
-    ds.remove_first("not found")
-    print(ds) # unchanged
-    ds.remove_first("strawberry")
-    print(ds) # [orange->banana->strawberry->apple]
-    ds.remove_first("strawberry")
-    print(ds) # [orange->banana->apple]
-    ds.remove_first("strawberry")
-    print(ds) # unchanged
-    ds.remove_at(0)
-    print(ds) # [banana->apple]
-    ds.remove_at(1)
-    print(ds) # [banana]
-    ds.add("pineapple", 0)
-    ds.add("tomato", 0)
-    print(ds) # [tomato->pineapple->banana]
-    print(f'Node at position 0: {ds.get(0)}')
-    ds.clear()
-    print(ds) # []
+    ll.add("apple", 0)
+    print(ll) # [apple]
+    print('head:', ll.peak()) # apple
+
+    ll.add("orange", 0)
+    print(ll) # [orange->apple]
+    print('head:', ll.peak())  # orange
+
+    ll.add("strawberry", 1)
+    print(ll) # [orange->strawberry->apple]
+    print(f'Node at position 2: {ll.get(2)}')
+
+    ll.add("banana", 2)
+    print(ll) # [orange->strawberry->banana->apple]
+
+    ll.add("strawberry", 3)
+    print(ll) # [orange->strawberry->banana->strawberry->apple]
+    print(f'Node at position 3: {ll.get(3)}')
+    ll.remove_first("not found")
+    print(ll) # unchanged
+
+    ll.remove_first("strawberry")
+    print(ll) # [orange->banana->strawberry->apple]
+
+    ll.remove_first("strawberry")
+    print(ll) # [orange->banana->apple]
+    ll.remove_first("strawberry")
+    print(ll) # unchanged
+
+    ll.remove_at(0)
+    print(ll) # [banana->apple]
+
+    ll.remove_at(1)
+    print(ll) # [banana]
+
+    ll.add("pineapple", 0)
+    ll.add("tomato", 0)
+    print(ll) # [tomato->pineapple->banana]
+    print(f'Node at position 0: {ll.get(0)}')
+    print('isEmpty ?:', ll.is_empty())  # False
+
+    ll.clear()
+    print('isEmpty ?:', ll.is_empty())  # True
+    print(ll) # []
 
 test_linked_list()
